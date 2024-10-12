49eedf9c561f462635b8d7ccc7d31ad1136f10ea
AlAminKh03 <alaminkhan6203@gmail.com>
2024-10-13 00:23:01 +0600

    initial commit

---
+++ src/components/Square.tsx
@@ -0,0 +1,12 @@
+const Square = ()=>{
+    const squares=[{
+        id: 1,
+    }]
+    return <>
+    <div className="bg-white  text-center w-10 h-10 border border-gray-700 ">
+    <button className=" bg-white  text-center w-10 h-10 border border-gray-700">X</button>
+    </div>
+    </>
+}
+
+export default Square ;
